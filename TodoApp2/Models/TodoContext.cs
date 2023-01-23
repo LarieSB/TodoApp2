07125cfaf7f694cc6bff8897c12889d0faf548c7
LarieSB <110356103+LarieSB@users.noreply.github.com>
2023-01-23 23:15:07 +0100

    Todo App
    Backend with ASP.net Core 6 und Frontend with vuejs

---
+++ TodoApp2/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp2.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+          
+        
+        public DbSet<Todo> Todos { get; set; }
+
+    
+    
+    
+    }
+}
---
+++ TodoApp2/Models/Todos.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp2.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Completed { get; set; }
+
+    }
+}
